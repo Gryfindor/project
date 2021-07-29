8e3df66767aea35f28b9a13e55d97d7409bc51d4
Jasza <wrapper888@gmail.com>
2021-07-29 04:42:39 +0200

    functions.js added

---
+++ lesson_1/js/functions.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// functions
+
+function calc(a, b) {
+    return a + b;
+}
+
+console.log(calc(5, 4));
+console.log(calc(56, 21));
+console.log(calc(15, 32));
+
+
+let showMessage = function () {
+    console.log("Hello!"); 
+};
+
+showMessage();
+
+const returner = (a, b) => a + b; // arrow func
+
+console.log(returner(5, 4));
+console.log(returner(100, 200));
